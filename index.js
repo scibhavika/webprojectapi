fcbf19468b3f236764b33e47bed84d91488835da
bhavika-at-757940286578 <bhavika@scinotic.com>
2022-08-03 12:40:18 +0530

    some modification in index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import express from "express";
+import cors from "cors";
 const express = require("express");
 const cors = require("cors");
  
@@ -6,7 +8,7 @@ const app=express();
 app.use(cors());
 const port = process.env.PORT || 3000;
 
-const apiData = require("./data.json");
+import apiData from "./data.json";
 app.get("/",(req,res)=>{
     res.send("we are live");
 });
